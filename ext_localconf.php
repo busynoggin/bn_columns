3c1eb3b8eb8620cee5623a9b750842b0711d9a69
Jeff Segars <jeff@busynoggin.com>
2016-05-02 11:53:27 -0500

    Add TYPO3 6.2 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,13 +3,15 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon('fluidcontent-bn_columns_TwoColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
-    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/TwoColumnGroup.svg'
-));
-$iconRegistry->registerIcon('fluidcontent-bn_columns_ThreeColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
-    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/ThreeColumnGroup.svg'
-));
-$iconRegistry->registerIcon('fluidcontent-bn_columns_FourColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
-    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/FourColumnGroup.svg'
-));
+if (FALSE === version_compare(TYPO3_version, '7.6', '<')) {
+	$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+	$iconRegistry->registerIcon('fluidcontent-bn_columns_TwoColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
+	    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/TwoColumnGroup.svg'
+	));
+	$iconRegistry->registerIcon('fluidcontent-bn_columns_ThreeColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
+	    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/ThreeColumnGroup.svg'
+	));
+	$iconRegistry->registerIcon('fluidcontent-bn_columns_FourColumnGroup_html', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
+	    'source' => 'EXT:bn_columns/Resources/Public/Icons/Content/FourColumnGroup.svg'
+	));
+}
